72912cb4b5421a085e9ee4eb262536e1af0b13af
ReginaF2012 <skritschy@gmail.com>
2020-07-02 14:11:15 -0500

    Users can log in with google, created private log_in method for sessionscontroller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -13,13 +13,12 @@ class SessionsController < ApplicationController
         #If the user is logging in via omniauth
         if auth_hash = request.env['omniauth.auth']
             @user = User.find_or_create_by_auth_hash(auth_hash)
-            session['user_id'] = @user.id
-            redirect_to user_path(@user)
+            log_in
         else
             #byebug
             @user =  User.find_by(email: params[:email])
             if @user.authenticate(params[:password])
-                redirect_to user_path(@user)
+                log_in
             else
                 render 'new'
             end
@@ -29,6 +28,13 @@ class SessionsController < ApplicationController
     def destroy
         session.delete :user_id
         redirect_to root_path
+    end
+
+    private
+
+    def log_in
+        session['user_id'] = @user.id
+        redirect_to user_path(@user)
     end
     
 end
