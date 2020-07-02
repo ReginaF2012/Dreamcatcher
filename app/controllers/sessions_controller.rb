19aaa68a57372d634c4d8748de3bc484026428e6
ReginaF2012 <skritschy@gmail.com>
2020-07-02 12:49:30 -0500

    created find_or_create_by_auth_hash method for User class, used that in controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,18 +6,9 @@ class SessionsController < ApplicationController
     def create
         #If the user is logging in via omniauth
         if auth_hash = request.env['omniauth.auth']
-            #see if user is already in the database
-            if user = User.find_by(email: auth_hash['info']['email'])
-                #log them in
-                session['user_id'] = user.id
-                redirect_to user_path(user)
-            else
-                #create a new user
-                user = User.new(email: auth_hash['info']['email'], password: SecureRandom.hex)
-                user.save
-                session['user_id'] = user.id
-                redirect_to user_path(user)
-            end
+            @user = User.find_or_create_by_auth_hash(auth_hash)
+            session['user_id'] = @user.id
+            redirect_to user_path(@user)
         else
 
         end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,16 @@ class User < ApplicationRecord
     has_many :dreams, dependent: :destroy
     has_many :symbolisms, through: :dreams
     has_secure_password
-    
+
+    def self.find_or_create_by_auth_hash(auth_hash)
+        #see if user is already in the database
+        if user = self.find_by(email: auth_hash['info']['email'])
+            user
+        else
+            #create a new user
+            user = self.new(email: auth_hash['info']['email'], password: SecureRandom.hex)
+            user.save
+            user
+        end
+    end
 end
