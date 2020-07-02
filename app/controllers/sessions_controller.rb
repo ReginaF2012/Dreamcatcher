dd139a20db497a9db3bfe3973d9c277daad01a61
ReginaF2012 <skritschy@gmail.com>
2020-07-01 20:09:34 -0500

    user can signup via google with omniauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,11 +1,28 @@
 class SessionsController < ApplicationController
-    
+
     def new
     end
 
     def create
+        #If the user is logging in via omniauth
+        if auth_hash = request.env['omniauth.auth']
+            #see if user is already in the database
+            if user = User.find_by(email: auth_hash['info']['email'])
+                #log them in
+                session['user_id'] = user.id
+                redirect_to user_path(user)
+            else
+                #create a new user
+                user = User.new(email: auth_hash['info']['email'], password: SecureRandom.hex)
+                user.save
+                session['user_id'] = user.id
+                redirect_to user_path(user)
+            end
+        else
+
+        end
     end
-    
+
     def destroy
     end
     
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
     has_many :dreams, dependent: :destroy
     has_many :symbolisms, through: :dreams
     has_secure_password
+    
 end
