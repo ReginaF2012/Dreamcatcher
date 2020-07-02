efd126d863ae50e0132857b303fdb784ec710e5f
ReginaF2012 <skritschy@gmail.com>
2020-07-02 17:36:06 -0500

    created a find_unique_username method for User class, now when signed in via google it automatically makes a unique username from the user's name given by omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,32 @@
 class User < ApplicationRecord
+    validates :username, presence: :true, uniqueness: :true
+    validates :email, presence: :true, uniqueness: :true
     has_many :dreams, dependent: :destroy
     has_many :symbolisms, through: :dreams
     has_secure_password
 
     def self.find_or_create_by_auth_hash(auth_hash)
-        self.find_by(email: auth_hash['info']['email']) ? self.find_by(email: auth_hash['info']['email']) :  self.create(email: auth_hash['info']['email'], password: SecureRandom.hex)
+        if self.find_by(email: auth_hash['info']['email']) 
+            self.find_by(email: auth_hash['info']['email'])
+        else 
+            username = self.find_unique_username(auth_hash['info']['name'])
+            self.create(email: auth_hash['info']['email'], username: username, password: SecureRandom.hex)
+        end
     end
+
+    # https://alexcastano.com/generate-unique-usernames-for-ruby-on-rails/
+    # ^^^ Link to where I found inspiration for this method ^^^
+    def self.find_unique_username(name)
+        username = name.parameterize(separator: '_')
+        taken_usernames = User.where("username LIKE ?", "#{username}%").pluck(:username)
+
+        count = 1
+        while taken_usernames.include?(username)
+            username = name.parameterize(separator: '_')
+            username = "#{username}_#{count}"
+            count += 1
+        end
+        username
+      end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_01_161907) do
+ActiveRecord::Schema.define(version: 2020_07_02_194925) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 2020_07_01_161907) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "dream_symbolisms", "dreams"
