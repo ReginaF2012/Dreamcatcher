fe41b32e54b514d24e87a2d4685869a492ce0b3c
ReginaF2012 <skritschy@gmail.com>
2020-07-03 17:13:35 -0500

    stubbed out routes for dreams_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'static#home'
-  resources :users, only: [:show, :create]
+  resources :users, only: [:show, :create] do
+    resources :dreams
+  end
+  resources :dreams, only: [:show, :index]
   get '/auth/:provider/callback', to: 'sessions#create'
   get '/signup', to: 'users#new', as: 'signup'
   get '/login', to: 'sessions#new', as: 'login'
